5892b3dd08cb50f48f116298f6cadd65931eb17e
Kostya Likhachev <arrow.koss@gmail.com>
2019-03-07 12:00:00 +0200

    - added button menu and title - added constraints - added image slide

--- RecipePizza/RecipePizza/ViewController.swift
+++ RecipePizza/RecipePizza/ViewController.swift
@@ -9,19 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var imageView: UIImageView!
+    
+    let headerImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "RectHeader"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let headerPizzaImageView: UIView = {
+        let imageView = UIImageView(image: UIImage(named: "slide"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.imageViewCorners()
+        return imageView
+    }()
+    
+    let menuButton: UIButton = {
+        let button = UIButton(type: .system)
+        let imageButton = UIImage(named: "menu")
+        button.setImage(imageButton?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let titleHeader: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Helvetica", size: 25)
+        label.text = "Рецепты пиццы"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 375, height: self.view.frame.width))
-        imageView.image = UIImage(named: "RectHeader")
-        imageView.contentMode = .scaleAspectFill
+        view.addSubview(headerImageView)
+        view.addSubview(headerPizzaImageView)
+        view.addSubview(menuButton)
+        view.addSubview(titleHeader)
+        ViewConstraints()
         
-        self.view.addSubview(imageView)
     }
+    
+    func ViewConstraints() {
+        
+        // headerImageView constraints
+        headerImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        headerImageView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        headerImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        // headerPizzaImageView cotsntraints
+        headerPizzaImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        headerPizzaImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        headerPizzaImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        
+        // menuButton cotsntraints
+        menuButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
+        menuButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        menuButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        menuButton.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        // titleHeader cotsntraints
+        titleHeader.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        titleHeader.topAnchor.constraint(equalTo: view.topAnchor, constant: 45).isActive = true
+        titleHeader.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+}
 
 
+extension UIImageView {
+    func imageViewCorners() {
+        layer.cornerRadius = 10
+        layer.borderWidth = 1.0
+        layer.masksToBounds = true
+    }
 }
 
